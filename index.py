3e83ea2936fda1f5c17f6d22d891636d9a36b597
Igor <4456050@gmail.com>
2023-11-07 16:42:11 +0300

    add commands for git

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
+# git status - проверить статус изменений
+# git add -A  - добавить изменения в пространство перед отправкой
+# git commit -m "NAME COMMIT" - дать название коммиту. в кавычка название соответствующее изменению например "fix strings"
+# git push - загрузить изменения
+# git pull - выгрузить изменения из github
+
+
 print('Hello, World!!!!!')
 print('Hello, Worlfffffffd!')
